b1d34de3af02b48a29aa760573b2a4b1ba969a67
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-01-30 20:35:48 +0200

    Add files via upload

---
+++ JS-Basics/06. Concatenate-Data.js
@@ -0,0 +1,8 @@
+function concatenateData(input){
+    let firstName = input[0];
+    let lastName = input[1];
+    let userAge = input[2];
+    let userLocation = input[3];
+
+    console.log(`You are ${firstName} ${lastName}, a ${userAge}-years old person from ${userLocation}.`);
+}
