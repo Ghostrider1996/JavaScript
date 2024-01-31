87afcecb3946240bcc47032011e9fb676a985a7f
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-01-31 14:49:26 +0200

    Add files via upload

---
+++ JS-Fundamentals/01-JS-Basics-Recap/More-Exercises/04-Reverse-String.js
@@ -0,0 +1,9 @@
+function moreExercise4(word) {
+  let wordToArray = word.split("");
+  let reversedWord = wordToArray.reverse().join("");
+
+  console.log(reversedWord);
+}
+moreExercise4("Hello");
+moreExercise4("SoftUni");
+moreExercise4("1234");
