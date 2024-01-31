9bfd39faab52c6a087daf10c1ac2ead0c2283266
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-01-31 15:07:09 +0200

    Add files via upload

---
+++ JS-Fundamentals/02-Data-Types-and-Variables/More-Exercises/01-Digits-with-Words.js
@@ -0,0 +1,22 @@
+function moreExercise1(digit) {
+  let digitName = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  let result = digitName[digit];
+
+  console.log(result);
+}
+
+moreExercise1("nine");
+moreExercise1("two");
+moreExercise1("zero");
