6fcbbce8f64b5965c218f33d7aa2b7c7c8e1cb9d
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-02-01 21:11:50 +0200

    d

--- JS-Fundamentals/03-Arrays/Exercises/10-Ladybugs.js
+++ JS-Fundamentals/03-Arrays/Exercises/10-Ladybugs.js
@@ -52,6 +52,6 @@ function exercise10(arr) {
   }
   console.log(field.join(" "));
 }
-exercise10([3, "0 1", "0 right 1", "2 right 1"]);
+// exercise10([3, "0 1", "0 right 1", "2 right 1"]);
 // exercise10([3, "0 1 2", "0 right 1", "1 right 1", "2 right 1"]);
 // exercise10([5, "3", "3 left 2", "1 left -2"]);
