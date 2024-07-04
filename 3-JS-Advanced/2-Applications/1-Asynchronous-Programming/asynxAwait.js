81d925a09f8c225a220ff5c6791ca11a12d0f9f7
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-07-04 21:21:14 +0300

    Exercise 1

--- 3-JS-Advanced/2-Applications/1-Asynchronous-Programming/asynxAwait.js
+++ 3-JS-Advanced/2-Applications/1-Asynchronous-Programming/asynxAwait.js
@@ -1,10 +1,22 @@
-function pizzaMaker(pizza) {
-    
+function Repos() {
+  const button = document.getElementById("loadRepos").addEventListener("click", loadRepos);
 
+  function loadRepos() {
+    let url = "https://api.github.com/users/testnakov/repos";
 
-    async function make(pizza) {
-        let pizza = await fetch()
+    const httpRequest = new XMLHttpRequest();
+
+    httpRequest.addEventListener("readystatechange", onLoad);
+    httpRequest.open("GET", url);
+    httpRequest.send();
+
+    function onLoad() {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        const data = JSON.parse(httpRequest.responseText);
+        document.getElementById("res").innerHTML = data.map(r => `<li>${r.name}</li>`).join("");
+      }
     }
+  }
 }
 
-
+Repos();
